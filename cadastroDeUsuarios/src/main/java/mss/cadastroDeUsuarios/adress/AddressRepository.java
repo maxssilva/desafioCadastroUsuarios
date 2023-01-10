d4dd919cc6ea9674e27b752f46aed7f9442919ad
Max Silva <64077276+maxssilva@users.noreply.github.com>
2023-01-10 18:59:44 -0300

    Add files via upload
    deletando método em repository não utilizado

--- cadastroDeUsuarios/src/main/java/mss/cadastroDeUsuarios/adress/AddressRepository.java
+++ cadastroDeUsuarios/src/main/java/mss/cadastroDeUsuarios/adress/AddressRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
@@ -13,7 +12,6 @@ public interface AddressRepository extends JpaRepository<Address, Long> {
     Address findByCityAndStreetAndNumberAndUserId(String city, String street, Integer number, Long id);
 
     List<Address> findAllByUserId(Long userId);
-
-    Address findByCityAndStreetAndNumberAndMainAddress(String city, String street, Integer number, Boolean mainAddress);
+    
 }
 
