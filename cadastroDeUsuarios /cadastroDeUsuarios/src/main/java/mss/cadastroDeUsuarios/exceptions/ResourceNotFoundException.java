407d2558dab7c71bd5002c00712b0a15bd661d49
Max Silva <64077276+maxssilva@users.noreply.github.com>
2023-01-10 18:56:46 -0300

    Delete cadastroDeUsuarios /cadastroDeUsuarios directory

--- cadastroDeUsuarios /cadastroDeUsuarios/src/main/java/mss/cadastroDeUsuarios/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package mss.cadastroDeUsuarios.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    private static final long serialVersionUID=1L;
-    public ResourceNotFoundException(String message){
-        super (message);
-    }
-}
